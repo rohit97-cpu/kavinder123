bc3b8ddb2738f6d6963bfe935408028a6c9170f1
kaviner <rpanwar911@gmail.com>
2022-01-14 19:42:39 +0530

    commit 1

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { 
+  Text,
+  View,
+} from 'react-native';
+const App = () => { 
+  return (
+   <View>
+    <Text>Rohit </Text>
+    </View>
+  );
+};
+
+export default App;
